d79e03b02a65bd5927f1c09d888d788abe3cb9ca
Duncan Temple Lang <duncan@r-project.org>
2025-03-10 13:59:10 -0700

    parse the file/code if findAllCalls is given a string.

---
+++ R/basicFindCalls.R
@@ -0,0 +1,90 @@
+findAllCalls =
+function(code, pred = function(...) TRUE, skipIfFalse = TRUE,
+         walker = callFinder(pred, skipIfFalse = skipIfFalse))
+{
+
+    if(is.character(code)) {
+        if(file.exists(code))
+            code = parse(code)
+        else
+            code = parse(text = code)
+    }
+    
+    
+    walkCode(code, walker)
+    walker$ans()
+}
+
+skipFunctions =
+function(call, isName, ...)
+{
+   !isName || as.character(call[[1]]) != "function"
+}
+
+callFinder = 
+function (pred, ..., skipIfFalse = FALSE) 
+{
+    calls = list()
+    leaf = function(x, w, ...) {
+        ty = typeof(x)
+        if (ty %in% c("pairlist", "expression", "list", "language")) {
+            lapply(x, walkCode, w)
+            return(NULL)
+        }
+        else if (ty == "closure") {
+            walkCode(formals(x), w)
+            walkCode(body(x), w)
+            return(NULL)
+        }
+    }
+    call = function(x, w, ...) {
+        if (skipIfFalse && skipIfFalse(x, w)) 
+            return(NULL)
+        isName = is.name(x[[1]])
+        if (isSymbol(x[[1]], c("<-", "=")) && is.call(x[[2]])) 
+            attr(x[[2]], "isLHS") = TRUE
+        if (pred(x, isName, ...)) 
+            calls[[length(calls) + 1L]] <<- x
+        els = as.list(x)
+        if (isName && as.character(x[[1]]) %in% c(".Internal", 
+            ".Primitive")) 
+            els = els[-2]
+        for (ee in els) {
+            if (!missing(ee)) 
+                walkCode(ee, w)
+        }
+    }
+    list(handler = function(x, w) NULL, leaf = leaf, call = call, 
+        ans = function() calls)
+}
+
+skipIfFalse =
+function (x, w) 
+{
+    if (isCallTo(x, "if") && isFALSE(x[[2]])) {
+        if (length(x) == 4) 
+            walkCode(x[[4]], w)
+        return(TRUE)
+    }
+    FALSE
+}
+
+
+isCallTo =
+function (code, funName, indirect = character(),  #getIndirectCallFunList(), 
+    isLHS = NA) 
+{
+    if (is(code, "ScriptNodeInfo")) 
+        x = code@code
+    if (is(code, "R6")) 
+        is(code, "Call") && is_symbol(code$fn) && code$fn$value %in% 
+            funName
+    else (is.call(code) || is(code, "call")) && (isSymbol(code[[1]], funName)
+      #  || (!isFALSE(indirect) && isIndirectCall(code, 
+      #                                           indirect, funName, TRUE, isLHS = isLHS))
+    )
+}
+
+isSymbol =
+function (x, sym = character()) 
+    is.name(x) && (length(sym) == 0 || as.character(x) %in% sym)
